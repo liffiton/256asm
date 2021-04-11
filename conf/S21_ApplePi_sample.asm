bb4edad1c198a76294e8d6becdd3733663d837b4
Mark Liffiton <liffiton@gmail.com>
2021-04-10 23:05:10 -0500

    Add sample code for ApplePi.

---
+++ conf/S21_ApplePi_sample.asm
@@ -0,0 +1,13 @@
+# Example code, just demonstrating each instruction
+  add $2 $0
+  sub $3 $1
+  rand $4 5
+label:
+  load $6 $7
+  store $8 $9
+  jal label
+  jr $10
+  beq $11 label
+  bgt $12 label
+  set $13 $14
+  seti $15 16
