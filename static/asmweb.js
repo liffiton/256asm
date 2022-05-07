784139cfdaae95d39e1109acc6737a5de93002e9
Mark Liffiton <liffiton@gmail.com>
2022-05-06 20:52:35 -0500

    Regex tweaks + allow indented labels w/o error highlights.

--- static/asmweb.js
+++ static/asmweb.js
@@ -151,12 +151,12 @@ function setupCodeMirror() {
     CodeMirror.defineSimpleMode("256asm", {
         start: [
             // "sol" = match at start of line only
-            {token: "label",       regex: /[\w]*\:/i, sol: true, indent: true},
+            {token: "label",       regex: /[\s]*[a-z][\w]+\:/i, sol: true, indent: true},
             {token: "instruction", regex: instruction_regex}, // /(?:add|addi|sub|light|copy|zj)\b/i},
             {token: "register",    regex: register_regex}, // /\$[0-9a-z]+/i},
             {token: "number",      regex: /(?:0x|0b)[a-f\d]+|[-+]?(?:\.\d+|\d+\.?\d*)\b/i},
             {token: "comment",     regex: /#.*/},
-            {token: "target",      regex: /[a-z$][\w$]*/i},
+            {token: "target",      regex: /[a-z][\w]*/i},
             // simple way to catch a bunch of junk and highlight it...
             {token: "error",       regex: /[\S]+/},
         ],
