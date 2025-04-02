437ba9c21766f63134fbd482bf7985fc49eed178
Mark Liffiton <liffiton@gmail.com>
2025-04-02 11:38:57 -0500

    Add initial S25 arch.

---
+++ conf/S25_kamen_sample.asm
@@ -0,0 +1,10 @@
+# Meaningless code demonstrating each of the different instructions
+  add $1 $zero
+  move $1 $2
+  sub $2 $3
+  rand $4
+label1:
+  lb $5 $4
+  sb $4 $5
+  seti $6 127
+  bz $7 label1
